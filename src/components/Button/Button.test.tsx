68b52f277e97b7d1872970f125cf3f018f38ab07
RichCode <stanleyogada2000@gmail.com>
2023-02-27 16:09:05 +0100

    Remove App test
    - Add Button test

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,10 @@
+import { screen, render } from "@testing-library/react";
+import Button from "../Button";
+
+describe("Button", () => {
+  test("Renders Button and it the counter works", async () => {
+    render(<Button>Test Button</Button>);
+
+    screen.getByRole("button", { name: /test button/i });
+  });
+});
