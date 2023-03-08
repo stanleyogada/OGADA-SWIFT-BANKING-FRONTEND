85dc3c65560e61428ca0fea5b04023fa30674614
Techbrolakes <lekandar11@gmail.com>
2023-03-08 10:31:57 +0100

    ft:#6: setup the global theme configurations

---
+++ src/constants/index.ts
@@ -0,0 +1,27 @@
+// GLOBAL CONFGURATIONS FILE FOR THE APP
+const CLIENT_ROUTES = {
+  home: "/",
+  about: "/about",
+  rewards: "/rewards",
+  cards: "/cards",
+  profile: "/profile",
+};
+
+const COLORS = {
+  blue: "#006FFF",
+  white: "#FFFFFF",
+  black: "#000000",
+  purple: "#3A00FF",
+  gray: "#E8E8E8",
+  lightyellow: "#FFDDB5",
+  yellow: "#FF8800",
+  lightpink: "FFE4FD",
+  lightblue: "#B2D2F9",
+};
+
+const config = {
+  COLORS,
+  CLIENT_ROUTES,
+};
+
+export default config;
