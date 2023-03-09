1f3d92ce4d225ca8358d8a8ef68b4ef8ba249abe
Techbrolakes <lekandar11@gmail.com>
2023-03-09 20:22:59 +0100

    Trello: https://trello.com/c/R1Yw0YfU/3-plan-frontend-repo

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,23 +1,5 @@
-// GLOBAL CONFGURATIONS FILE FOR THE APP
-const CLIENT_ROUTES = {
-  home: "/",
-  about: "/about",
-  rewards: "/rewards",
-  cards: "/cards",
-  profile: "/profile",
-};
-
-const COLORS = {
-  blue: "#006FFF",
-  white: "#FFFFFF",
-  black: "#000000",
-  purple: "#3A00FF",
-  gray: "#E8E8E8",
-  lightyellow: "#FFDDB5",
-  yellow: "#FF8800",
-  lightpink: "FFE4FD",
-  lightblue: "#B2D2F9",
-};
+import { COLORS } from "./colors";
+import { CLIENT_ROUTES } from "./routes";
 
 const config = {
   COLORS,
