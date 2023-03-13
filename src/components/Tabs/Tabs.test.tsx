5eb9b37e83b50c13d5b7a7ab00bd8ebfe871f966
RichCode <stanleyogada2000@gmail.com>
2023-03-13 18:41:29 +0100

    ft-#12: Have Tabs logic tested

--- src/components/Tabs/Tabs.test.tsx
+++ src/components/Tabs/Tabs.test.tsx
@@ -1,10 +1,50 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Tabs from "./Tabs";
 
-describe("Tabs", () => {
-  test("Workings!", () => {
-    render(<Tabs />);
+const user = userEvent.setup();
 
-    screen.getByText(/tabs/i);
+beforeEach(() => {
+  render(
+    <Tabs
+      data={[
+        {
+          id: "tab-1",
+          heading: "Tab 1",
+        },
+        {
+          id: "tab-2",
+          heading: "Tab 2",
+        },
+      ]}
+    >
+      <div>Pane 1</div>
+      <div>Pane 2</div>
+    </Tabs>
+  );
+});
+
+describe("Tabs", () => {
+  test("renders props correctly", () => {
+    screen.getByText(/tab 1/i);
+    screen.getByText(/tab 2/i);
+    screen.getByText(/pane 1/i);
+    expect(screen.queryByText(/pane 2/i)).not.toBeInTheDocument();
+  });
+
+  test("renders one pane at a time", async () => {
+    screen.getByText(/pane 1/i);
+    expect(screen.queryByText(/pane 2/i)).not.toBeInTheDocument();
+
+    const tab1Control = screen.getByText(/tab 1/i);
+    const tab2Control = screen.getByText(/tab 2/i);
+
+    await user.click(tab2Control);
+    screen.getByText(/pane 2/i);
+    expect(screen.queryByText(/pane 1/i)).not.toBeInTheDocument();
+
+    await user.click(tab1Control);
+    screen.getByText(/pane 1/i);
+    expect(screen.queryByText(/pane 2/i)).not.toBeInTheDocument();
   });
 });
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,5 +1,48 @@
-const Tabs = () => {
-  return (<div>Tabs</div>)
-}
+import { useState } from "react";
+
+type TProps = {
+  data: Array<{
+    id: string;
+    heading: string;
+  }>;
+  children: Array<JSX.Element>;
+};
+
+const useTabs = (data: TProps["data"]) => {
+  const [activeId, setActiveId] = useState(data[0].id);
+
+  const handleChangeActiveId = (tabId: string) => {
+    setActiveId(tabId);
+  };
+
+  const getActiveIndex = () => {
+    return data.findIndex((tab) => tab.id === activeId);
+  };
+
+  console.log(activeId);
+
+  return {
+    handleChangeActiveId,
+    getActiveIndex,
+  };
+};
+
+const Tabs = ({ data, children }: TProps) => {
+  const { handleChangeActiveId, getActiveIndex } = useTabs(data);
+
+  return (
+    <section>
+      <header>
+        {data.map((tab) => (
+          <h2 key={tab.id} onClick={() => handleChangeActiveId(tab.id)}>
+            {tab.heading}
+          </h2>
+        ))}
+      </header>
+
+      <div>{children[getActiveIndex()]}</div>
+    </section>
+  );
+};
 
 export default Tabs;
