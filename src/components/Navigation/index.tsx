510fb1959eabb8119050c5b0c4bdc19f1dd07f22
CreatorXperience <allyearmustobey@gmail.com>
2023-06-19 07:03:22 -0700

    ft-#38: change all navigation routes to constants

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -11,28 +11,28 @@ const Navigation = () => {
       <Nav>
         <ul>
           <List>
-            <Link to="/">
+            <Link to={CLIENT_ROUTES.home}>
               <i>{icons.blackhomeIcon()}</i>
               <span>Home</span>
             </Link>
           </List>
 
           <List>
-            <Link to="/rewards">
+            <Link to={CLIENT_ROUTES.rewards}>
               <i>{icons.blackheartIcon()}</i>
               <span className="nav__name">Rewards</span>
             </Link>
           </List>
 
           <List>
-            <Link to="/">
+            <Link to={CLIENT_ROUTES.finance}>
               <i>{icons.blackchartIcon()}</i>
               <span>Finance</span>
             </Link>
           </List>
 
           <List>
-            <Link to="/">
+            <Link to={CLIENT_ROUTES.cards}>
               <i>{icons.blackcardIcon()}</i>
               <span>Cards</span>
             </Link>
