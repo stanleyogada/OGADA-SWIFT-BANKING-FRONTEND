da64fbf8f8191851050e65e6afda2acfc8a537f8
CreatorXperience <allyearmustobey@gmail.com>
2023-06-23 08:53:05 -0700

    ft-#31: complete page design

---
+++ src/pages/Account.tsx/index.tsx
@@ -0,0 +1,176 @@
+import styled from "styled-components";
+import icons from "../../constants/icons";
+import Avatar from "../../components/Avatar/Avatar";
+import { COLORS } from "../../constants";
+
+const Account = () => {
+  return (
+    <>
+      <AccountWrapper>
+        <div className="account-section">
+          <div className="header">
+            <div className="navigation-section">
+              <div className="back-icon"> {icons.blueleftarrowIcon()}</div>
+              <div className="section-title">Account Details</div>
+            </div>
+
+            <div className="edit-btn">Edit</div>
+          </div>
+
+          <div className="profile-section">
+            <div className="profile-title">Profile Homepage</div>
+
+            <div className="avatar">
+              <Avatar />
+              <div className="right-icon">{icons.bluerightarrowIcon()}</div>
+            </div>
+          </div>
+
+          <div className="user-info-section">
+            <div className="info-wrapper">
+              <div className="label">Name</div>
+              <div className="info">Egajivwie Samuel akpevwe</div>
+            </div>
+
+            <div className="info-wrapper">
+              <div className="label">OPay Account Number</div>
+              <div className="info">7025377623622</div>
+            </div>
+
+            <div className="info-wrapper">
+              <div className="label">Email</div>
+              <div className="info">
+                <button className="verified">Verified {icons.checkIcon()}</button>
+              </div>
+            </div>
+
+            <div className="expand">
+              <div className="show-wrapper">
+                <div className="show-wrapper-text"> Show More </div>
+                <div className="show-wrapper-icon">{icons.blueDropdownIcon()}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </AccountWrapper>
+    </>
+  );
+};
+
+export default Account;
+
+const AccountWrapper = styled.div`
+  .account-section {
+    width: 100%;
+    height: 100vh;
+
+    .header {
+      width: 100%;
+
+      height: auto;
+      display: flex;
+      justify-content: space-between;
+      padding: 45px 0px 0px 28.59px;
+
+      .navigation-section {
+        display: flex;
+
+        .back-icon {
+          margin-right: 19.8px;
+        }
+
+        .section-title {
+          color: ${COLORS.black};
+          font-size: 15px;
+          font-family: Inter;
+        }
+      }
+
+      .edit-btn {
+        margin-right: 17px;
+        color: ${COLORS.blue};
+        font-size: 15px;
+        font-family: Inter;
+      }
+    }
+
+    .profile-section {
+      width: 100%;
+      height: auto;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .profile-title {
+        margin-left: 29px;
+        color: ${COLORS.black};
+        font-size: 11px;
+        font-family: Inter;
+        font-weight: 700;
+      }
+
+      .avatar {
+        width: 20%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-right: 16.18px;
+      }
+    }
+
+    .user-info-section {
+      width: 100%;
+      height: 128px;
+      background-color: ${COLORS.blue};
+      padding: 9px 16px 9px 37px;
+      position: relative;
+
+      .info-wrapper {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 16px;
+        color: ${COLORS.white};
+        font-size: 11px;
+        font-family: Inter;
+
+        .info {
+          .verified {
+            width: 71px;
+            height: 18px;
+            flex-shrink: 0;
+            background-color: ${COLORS.white};
+            color: ${COLORS.blue};
+            font-size: 9px;
+            padding: 5px;
+            border: none;
+            border-radius: 10px;
+          }
+        }
+      }
+    }
+
+    .expand {
+      width: 100%;
+      height: 40px;
+      display: flex;
+      justify-content: center;
+      position: relative;
+      bottom: 0;
+
+      .show-wrapper {
+        display: flex;
+
+        .show-wrapper-text {
+          color: ${COLORS.white};
+          font-size: 11px;
+          font-family: Inter;
+        }
+
+        .show-wrapper-icon {
+          margin: -5px 0px 0px 5px;
+          flex-shrink: 0;
+        }
+      }
+    }
+  }
+`;
