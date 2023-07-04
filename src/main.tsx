6c530a887f41206b8087f95a2dfd0f7e9962897d
CreatorXperience <allyearmustobey@gmail.com>
2023-07-03 17:08:57 -0700

    ft-#54 wrapped app in React strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./main.css";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
