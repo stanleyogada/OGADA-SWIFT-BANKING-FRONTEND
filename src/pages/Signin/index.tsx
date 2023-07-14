e320ea19a6d800e57f418eea3e51dae9809e73a2
RichCode <stanleyogada2000@gmail.com>
2023-07-14 12:38:35 +0100

    ft-#28: Add style to page

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,48 +1,62 @@
 import { Link } from "react-router-dom";
 import useSignin from "./hooks/useSignin";
+import SigninWrapper from "./SigninWrapper";
+import Button from "../../components/Button";
 
 const Signin = () => {
   const { handleInputChange, handleSubmit, signInMutationState, formData } = useSignin();
 
   return (
-    <div>
-      <h1>Sign In</h1>
-
-      <h2>Welcome Back</h2>
-
-      {signInMutationState.isLoading && <p data-testid="loading"></p>}
+    <SigninWrapper>
       {signInMutationState.isError && <p data-testid="error"></p>}
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Phone Number"
-          name="phoneNumber"
-          onChange={handleInputChange}
-          value={formData.phoneNumber}
-        />
-        <input
-          type="password"
-          placeholder="Enter 6 digits login passcode"
-          name="loginPasscode"
-          onChange={handleInputChange}
-          value={formData.loginPasscode}
-        />
+      <header>
+        <h1 className="page-title">Sign In</h1>
+      </header>
 
-        <div>
-          <label htmlFor="remember-login-passcode">
-            <input type="checkbox" id="remember-login-passcode" />
-            Remember login passcode
-          </label>
+      <main className="content">
+        <h2 className="sub-title">Welcome Back!</h2>
 
-          <Link to="#">Forgot login passcode?</Link>
+        <form onSubmit={handleSubmit} className="form">
+          <div className="form__input-wrapper">
+            <input
+              type="text"
+              placeholder="Phone Number"
+              name="phoneNumber"
+              onChange={handleInputChange}
+              value={formData.phoneNumber}
+              className="form__input form__input--phone-number"
+            />
+            <input
+              type="password"
+              placeholder="Enter 6 digits login passcode"
+              name="loginPasscode"
+              onChange={handleInputChange}
+              value={formData.loginPasscode}
+              className="form__input form__input--login-passcode"
+            />
+          </div>
 
-          <button type="submit" disabled={signInMutationState.isLoading}>
-            Sign In
-          </button>
-        </div>
-      </form>
-    </div>
+          <div className="form__actions">
+            <div className="form__actions-top">
+              <label htmlFor="remember-login-passcode" className="form__checkbox">
+                <input type="checkbox" id="remember-login-passcode" />
+                Remember login passcode
+              </label>
+
+              <Link to="#" className="form__link">
+                Forgot login passcode?
+              </Link>
+            </div>
+
+            <Button type="submit" disabled={signInMutationState.isLoading} className="form__actions-bottom">
+              Sign In
+              {signInMutationState.isLoading && <p data-testid="loading"></p>}
+            </Button>
+          </div>
+        </form>
+      </main>
+    </SigninWrapper>
   );
 };
 
