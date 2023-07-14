06fc217b63dda59e60e10db1425f127ce9f172ce
RichCode <stanleyogada2000@gmail.com>
2023-07-14 12:31:21 +0100

    ft-#28: Cleanup component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,16 +3,35 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { COLORS } from "../../constants";
 
-interface ButtonProps {
+type TProps = {
   icon?: ReactNode;
   children?: ReactNode;
   link?: any;
   type?: "button" | "submit" | "reset";
   disabled?: boolean;
   className?: string;
-}
+};
 
-const SButton = styled.button`
+const Button = ({ icon, children, link, type, disabled, className }: TProps) => {
+  const btn = (
+    <ButtonWrapper type={type} disabled={disabled} className={className}>
+      {icon && <div data-testid="btn-icon">{icon}</div>}
+      <span>{children}</span>
+    </ButtonWrapper>
+  );
+
+  return link ? (
+    <LinkWrapper>
+      <Link data-testid="btn-link" to={link}>
+        {btn}
+      </Link>
+    </LinkWrapper>
+  ) : (
+    btn
+  );
+};
+
+const ButtonWrapper = styled.button`
   background-color: Transparent;
   display: flex;
   flex-direction: column;
@@ -34,22 +53,5 @@ const LinkWrapper = styled.div`
     text-decoration: none;
   }
 `;
-const Button: React.FC<ButtonProps> = ({ icon, children, link, type, disabled, className }) => {
-  const btn = (
-    <SButton type={type} disabled={disabled} className={className}>
-      {icon && <div data-testid="btn-icon">{icon}</div>}
-      <span>{children}</span>
-    </SButton>
-  );
 
-  return link ? (
-    <LinkWrapper>
-      <Link data-testid="btn-link" to={link}>
-        {btn}
-      </Link>
-    </LinkWrapper>
-  ) : (
-    btn
-  );
-};
 export default Button;
