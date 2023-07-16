b2946e67a76d435f3cc9cd5b0278db850b527e7c
RichCode <stanleyogada2000@gmail.com>
2023-07-16 23:19:11 +0100

    ft-#28: Cleanup test mocks dir

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect", "./src/utils/test/mocks.ts"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect", "./src/utils/test/mocks/index.ts"],
   moduleNameMapper: {
     "\\.(css|less|sass|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "identity-obj-proxy",
