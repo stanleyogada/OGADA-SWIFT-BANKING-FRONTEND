09136e8f72773a3620490ce043d59bd567f93ee8
CreatorXperience <allyearmustobey@gmail.com>
2023-07-19 06:18:10 -0700

    ft-#28: Remove useAuth hook

--- src/pages/Signup2/hooks/useSignup2.ts
+++ src/pages/Signup2/hooks/useSignup2.ts
@@ -1,12 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useForm } from "react-hook-form";
 
-import useAuth from "../../../hooks/useAuth";
-
 import type { TSignUpFormValues } from "../type";
+import { useMutation } from "react-query";
+import { postSignIn } from "../../../services/auth";
+import { AxiosError } from "axios";
 
 const useSignin = () => {
-  const { handleSignIn, signInMutationState } = useAuth();
+  const signInMutation = useMutation(postSignIn, {
+    onSuccess: (token: string) => {
+      localStorage.setItem("token", token); // TODO: remove this after fixing cookie issue on the backend
+      window.location.reload();
+    },
+  });
+
+  const handleSignIn = (phoneNumber: string, loginPasscode: string) => {
+    signInMutation.mutate({ phoneNumber, loginPasscode });
+  };
+
+  const signInMutationState = useMemo(
+    () => ({
+      isLoading: signInMutation.isLoading,
+      error: signInMutation.error as AxiosError,
+      isError: signInMutation.isError,
+    }),
+    [signInMutation.isLoading, signInMutation.error, signInMutation.isError]
+  );
+
   const {
     register,
     handleSubmit: _handleSubmit,
