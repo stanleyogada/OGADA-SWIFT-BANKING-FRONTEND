665251c5935c3e873e8740bc19b59f31307b30da
RichCode <stanleyogada2000@gmail.com>
2023-07-21 17:33:22 +0100

    ft-#28: Cleanup

--- src/pages/Signin/Signin.test.tsx
+++ src/pages/Signin/Signin.test.tsx
@@ -5,7 +5,7 @@ import { MemoryRouter } from "react-router-dom";
 
 import Signin from "./";
 import createServer from "../../utils/test/createServer";
-import { BASE_URL } from "../../constants/services";
+import { BASE_URL, ENDPOINTS } from "../../constants/services";
 
 const renderComponent = () => {
   const queryClient = new QueryClient({
@@ -30,7 +30,7 @@ const renderComponent = () => {
 const { handleCreateErrorConfig } = createServer([
   {
     method: "post",
-    url: `${BASE_URL}/auth/signin`,
+    url: `${BASE_URL}${ENDPOINTS.signIn}`,
     res() {
       return {
         token: "1234567890",
@@ -38,7 +38,7 @@ const { handleCreateErrorConfig } = createServer([
     },
   },
   {
-    url: `${BASE_URL}/users/me`,
+    url: `${BASE_URL}${ENDPOINTS.currentUser}`,
     res() {
       return {
         data: {
@@ -48,33 +48,6 @@ const { handleCreateErrorConfig } = createServer([
     },
   },
 ]);
-
-test("Render content of Signin page correctly", () => {
-  renderComponent();
-
-  const pageTitle = screen.getByRole("heading", { name: /sign in/i });
-  const subTitle = screen.getByRole("heading", { name: /welcome back/i });
-
-  const phoneInput = screen.getByPlaceholderText(/phone number/i);
-  const passwordInput = screen.getByPlaceholderText(/enter 6 digits login passcode/i);
-
-  const rememberLoginPasscode = screen.getByRole("checkbox", { name: /remember login passcode/i });
-  const forgotLoginPasscode = screen.getByRole("link", { name: /forgot login passcode/i });
-
-  const signInButton = screen.getByRole("button", { name: /sign in/i });
-
-  const goToSignup = screen.getByRole("link", { name: /don\'t have an account\? sign up/i });
-  expect(goToSignup).toHaveAttribute("href", "/auth/signup");
-
-  expect(pageTitle).toBeInTheDocument();
-  expect(subTitle).toBeInTheDocument();
-  expect(phoneInput).toBeInTheDocument();
-  expect(passwordInput).toBeInTheDocument();
-  expect(rememberLoginPasscode).toBeInTheDocument();
-  expect(forgotLoginPasscode).toBeInTheDocument();
-  expect(signInButton).toBeInTheDocument();
-  expect(goToSignup).toBeInTheDocument();
-});
 
 const handleAssertTypeInForm = async (
   user: ReturnType<typeof userEvent.setup>,
@@ -103,20 +76,31 @@ const handleAssertLoadingAfterSubmitClick = async () => {
   expect(signInButton).not.toBeDisabled();
 };
 
-test("Signing form works correctly onLoading", async () => {
-  const user = userEvent.setup();
+test("Render content of Signin page correctly", () => {
   renderComponent();
 
-  await handleAssertTypeInForm(user, { phone: "1234567890", loginPasscode: "123456" });
+  const pageTitle = screen.getByRole("heading", { name: /sign in/i });
+  const subTitle = screen.getByRole("heading", { name: /welcome back/i });
 
-  expect(screen.queryByTestId("loading")).not.toBeInTheDocument();
+  const phoneInput = screen.getByPlaceholderText(/phone number/i);
+  const passwordInput = screen.getByPlaceholderText(/enter 6 digits login passcode/i);
+
+  const rememberLoginPasscode = screen.getByRole("checkbox", { name: /remember login passcode/i });
+  const forgotLoginPasscode = screen.getByRole("link", { name: /forgot login passcode/i });
 
   const signInButton = screen.getByRole("button", { name: /sign in/i });
-  expect(signInButton).not.toBeDisabled();
 
-  await user.click(signInButton);
+  const goToSignup = screen.getByRole("link", { name: /don\'t have an account\? sign up/i });
+  expect(goToSignup).toHaveAttribute("href", "/auth/signup");
 
-  await handleAssertLoadingAfterSubmitClick();
+  expect(pageTitle).toBeInTheDocument();
+  expect(subTitle).toBeInTheDocument();
+  expect(phoneInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
+  expect(rememberLoginPasscode).toBeInTheDocument();
+  expect(forgotLoginPasscode).toBeInTheDocument();
+  expect(signInButton).toBeInTheDocument();
+  expect(goToSignup).toBeInTheDocument();
 });
 
 test("Signing form works correctly onSuccess", async () => {
