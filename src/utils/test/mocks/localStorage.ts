19509c43613bd1eafad303601df411b8ce5f3bda
RichCode <stanleyogada2000@gmail.com>
2023-07-22 10:28:30 +0100

    ft-#28: Fix signin failing test

--- src/utils/test/mocks/localStorage.ts
+++ src/utils/test/mocks/localStorage.ts
@@ -1,15 +1,18 @@
 const localStorageSetItem = jest.fn();
+const localStorageRemoveItem = jest.fn();
 
 beforeEach(() => {
   Object.defineProperty(window, "localStorage", {
     value: {
       setItem: (key: string, value: string) => localStorageSetItem(key, value),
+      removeItem: (key: string) => localStorageRemoveItem(key),
     },
   });
 });
 
 afterEach(() => {
   localStorageSetItem.mockClear();
+  localStorageRemoveItem.mockClear();
 });
 
-export { localStorageSetItem };
+export { localStorageSetItem, localStorageRemoveItem };
