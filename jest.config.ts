4e0cebd5d4cb06cea8975b9a7b3e09d922fcaae6
RichCode <stanleyogada2000@gmail.com>
2023-07-25 17:27:07 +0100

    rf-#59: Add component to path

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|less|sass|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "identity-obj-proxy",
-    "src/(.*)": "<rootDir>/src/$1",
+    "@components/(.*)": "<rootDir>/src/components/$1",
   },
   preset: "ts-jest",
   transform: {
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      src: "/src",
+      "@components": "/src/components",
     },
   },
   optimizeDeps: {
