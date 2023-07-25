c08b69a0cc30a6550bcf54c51cbd064c5c80970a
RichCode <stanleyogada2000@gmail.com>
2023-07-25 17:47:11 +0100

    rf-#59: Add all paths aliased for jest and typescript

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,22 @@
+const getAliases = () => {
+  const paths = ["assets", "components", "constants", "hooks", "pages", "services", "types", "utils"];
+
+  const aliases = paths.reduce((acc, path) => {
+    return {
+      ...acc,
+      [`@${path}/(.*)`]: `<rootDir>/src/${path}/$1`,
+    };
+  }, {});
+
+  return aliases;
+};
+
 module.exports = {
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect", "./src/utils/test/mocks/index.ts"],
   moduleNameMapper: {
     "\\.(css|less|sass|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "identity-obj-proxy",
-    "@components/(.*)": "<rootDir>/src/components/$1",
+    ...getAliases(),
   },
   preset: "ts-jest",
   transform: {
