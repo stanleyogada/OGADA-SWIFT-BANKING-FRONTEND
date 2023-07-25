409f38781c5f394683a5b4fa43094633d486d598
RichCode <stanleyogada2000@gmail.com>
2023-07-25 17:33:29 +0100

    rf-#59: Add all paths aliased for vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 const path = require("path");
 
+const getAliases = () => {
+  const paths = ["assets", "components", "constants", "hooks", "pages", "services", "types", "utils"];
+
+  const aliases = paths.reduce((acc, path) => {
+    return {
+      ...acc,
+      [`@${path}`]: `/src/${path}`,
+    };
+  }, {});
+
+  return aliases;
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@components": "/src/components",
+      ...getAliases(),
     },
   },
   optimizeDeps: {
