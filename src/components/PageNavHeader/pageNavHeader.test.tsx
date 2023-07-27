8687653544bdbdb2412ce62bdad16d297a6ccf02
goodness <goodyc474@gmail.com>
2023-07-28 00:03:52 +0100

    ft-#61: test  pageNavHeader component

---
+++ src/components/PageNavHeader/pageNavHeader.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { useNavigate } from "react-router-dom";
+
+import PageNavHeader from "./pageNavHeader";
+
+const renderComponent = () => {
+  render(<PageNavHeader heading="name" backgroundColor="red" />);
+};
+
+describe("render pageNavHeader correctly", () => {
+  test("should display the header correctly", () => {
+    renderComponent();
+    const headingElement = screen.getByRole("heading");
+    const arrowElement = screen.getByTitle("left-caret");
+
+    expect(arrowElement).toBeInTheDocument();
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test("should Have the padding styles provided correctly", () => {
+    renderComponent();
+    const divElement = screen.getByTitle("left-caret");
+
+    expect(divElement.parentNode).toHaveStyle("padding: 50px 0px 15px 29px");
+  });
+
+  test("should Go back to previous page on click on the left-caret icon", async () => {
+    const user = userEvent.setup();
+    const navigate = useNavigate();
+
+    renderComponent();
+    const iconElement = screen.getByTestId("icon");
+    await user.click(iconElement);
+    expect(navigate).toHaveBeenCalledWith(-1);
+  });
+});
