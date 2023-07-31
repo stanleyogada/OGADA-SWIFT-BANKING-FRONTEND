3282de3593a6efda631b149bd06e9985bffffe13
RichCode <stanleyogada2000@gmail.com>
2023-07-31 18:57:38 +0100

    ft-#65: Cleanup imports

--- src/pages/Auth/ForgetPassword/ForgetPasswordWrapper.ts
+++ src/pages/Auth/ForgetPassword/ForgetPasswordWrapper.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import { COLORS } from "../../../constants/colors/index";
+import { COLORS } from "@constants/colors";
 
 const ForgetPasswordWrapper = styled.div`
   .page-title {
