b018606aba767ace8ac3ad5a0b20074ae545da90
CreatorXperience <allyearmustobey@gmail.com>
2023-08-10 14:04:23 +0100

    ft-#69: infinite-query init

--- src/pages/TransactionPage/index.tsx
+++ src/pages/TransactionPage/index.tsx
@@ -1,18 +1,43 @@
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
+import { Fragment, useState } from "react";
 import { getTransactions } from "@services/transaction";
 const Transaction = () => {
-  let { data } = useQuery("key", getTransactions, {
-    enabled: true,
-  });
+  let { data, hasNextPage, fetchNextPage, isFetching, isFetchingNextPage } = useInfiniteQuery(
+    ["transaction"],
+    () => getTransactions({ pageNumber: 1 }),
+    {
+      getNextPageParam: (_lastPage, page) => {
+        if (page.length < 2) {
+          return page.length + 1;
+        } else {
+          return undefined;
+        }
+      },
+    }
+  );
 
-  console.log(data);
   return (
     <div>
       <ul>
-        {data?.map((d: { type: string; T_id: number }) => {
-          return <li key={d.T_id * 2}>{d.type}</li>;
+        {data?.pages.map((group, i) => {
+          return (
+            <Fragment key={i}>
+              {group.data.map((data: { T_id: number; type: string }) => {
+                return <li>{data.T_id}</li>;
+              })}
+            </Fragment>
+          );
         })}
       </ul>
+      <button
+        onClick={() => {
+          fetchNextPage();
+        }}
+        disabled={!hasNextPage}
+      >
+        load more
+      </button>
+      <div>{isFetching && isFetchingNextPage ? "Fetching more" : null}</div>
     </div>
   );
 };
--- src/services/transaction/index.ts
+++ src/services/transaction/index.ts
@@ -1,8 +1,7 @@
 import { transactionInstance } from "@utils/axiosInstance";
 
-const getTransactions = async () => {
-  let { data } = await transactionInstance.get("/trans");
-  return data;
+const getTransactions = async ({ pageNumber = 1 }) => {
+  return await transactionInstance.get(`/trans?_limit=4&_page=${pageNumber}`);
 };
 
 export { getTransactions };
