387eac2d97efb30991d4490192b46b8a1f1c95a7
CreatorXperience <allyearmustobey@gmail.com>
2023-09-06 18:00:15 +0100

    ft-#69: fix all failing test concerning date

--- src/pages/TransactionPage/Transaction.test.tsx
+++ src/pages/TransactionPage/Transaction.test.tsx
@@ -10,7 +10,7 @@ import dayjs from "dayjs";
 const response = [
   {
     transaction_id: 21,
-    created_at: new Date().toISOString(),
+    created_at: "2023-09-02T21:57:18.999Z",
     transaction_type: "in-house",
     amount: "280.00",
     is_success: true,
@@ -21,7 +21,7 @@ const response = [
   },
   {
     transaction_id: 21,
-    created_at: "2023-09-02T21:57:18.999Z",
+    created_at: "2023-09-04T21:57:18.999Z",
     transaction_type: "banks",
     amount: "280.00",
     is_success: false,
@@ -73,24 +73,15 @@ test("Renders two cards on load and fetches more data on button click", async ()
 
   const initialCards = await screen.findAllByTestId("transaction-card");
   expect(initialCards).toHaveLength(4);
+  expect(initialCards[0]).toHaveTextContent(new RegExp(`Daily in-house`));
+  expect(initialCards[0]).toHaveTextContent(/saturday, sep 2023/i);
+  expect(initialCards[0]).toHaveTextContent(/\+N280.00/);
+  expect(initialCards[0]).toHaveTextContent(/successful/);
 
-  //TODO: will remove this after fixing date test
-  //  expect(initialCards[i]).toHaveTextContent(/sep 2,  2023 10:57 PM/);
-
-  for (let i = 0; i < response.length; i++) {
-    expect(initialCards[i]).toHaveTextContent(new RegExp(`Daily ${response[i].transaction_type}`));
-
-    if (!response[i].is_deposit) {
-      expect(initialCards[i]).toHaveTextContent(new RegExp("-N280.00"));
-    } else {
-      expect(initialCards[i]).toHaveTextContent(new RegExp("N280.00"));
-    }
-    if (response[i].is_success) {
-      expect(initialCards[i]).toHaveTextContent(/successful/);
-    } else {
-      expect(initialCards[i]).toHaveTextContent("failed");
-    }
-  }
+  expect(initialCards[1]).toHaveTextContent(new RegExp(`Daily banks`));
+  expect(initialCards[1]).toHaveTextContent(/monday, sep 2023/i);
+  expect(initialCards[1]).toHaveTextContent(/-N280.00/);
+  expect(initialCards[1]).toHaveTextContent(/failed/);
 
   const user = userEvent.setup();
 
