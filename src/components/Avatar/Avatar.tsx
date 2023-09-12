6677ce7549f3a5f4a687cfdc649fa5327acf73a7
RichCode <stanleyogada2000@gmail.com>
2023-09-12 07:11:29 +0100

    ft-#76: Remove log statement

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,8 +2,6 @@ import ImageWrapper from "./AvatarStyle";
 import { DEFAULT_USER_AVATAR } from "@constants/index";
 
 const Avatar = ({ src, alt }: { src?: string; alt?: string }) => {
-  console.log(alt);
-
   return (
     <ImageWrapper>
       <img src={src || DEFAULT_USER_AVATAR} alt={alt} width={50} height={50} />
