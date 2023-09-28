732e7f898e08c8c73fd5d3cbed29b7ddd75a83dc
RichCode <stanleyogada2000@gmail.com>
2023-09-28 19:06:53 +0100

    ft-#74: Allow users to see list of banks to choose from
    - Allow users to verify success
    - Prompt error on verify failure

--- src/pages/SendMoneyBank/index.tsx
+++ src/pages/SendMoneyBank/index.tsx
@@ -1,30 +1,68 @@
 import { DEFAULT_BANK_LOGO } from "@constants/index";
 import useSendMoneyBank from "./hooks/useSendMoneyBank";
+import { TBank } from "@services/banks/types";
 
 const SendMoneyBank = () => {
-  const { banks } = useSendMoneyBank();
+  const {
+    verifyAccount,
+    banks,
+    currentBank,
+    recipientAccountNumberInputIsDisabled,
+    isRecipientFound,
+    handleCurrentBankCodeChange,
+    register,
+  } = useSendMoneyBank();
 
   return (
     <div>
       <input
         type="text"
         placeholder="Recipient account number"
-        // {...register("recipientAccountNumber")}
+        maxLength={10}
+        disabled={recipientAccountNumberInputIsDisabled}
+        {...register("recipientAccountNumber")}
       />
 
-      <div>
-        <h2>Select a bank</h2>
+      {currentBank && (
+        <div data-testid="current-bank">
+          <Bank bank={currentBank} />
 
-        {banks.isLoading && <div data-testid="get-all-banks-loading">Loading banks...</div>}
+          <button data-testid="remove" onClick={() => handleCurrentBankCodeChange(null)}>
+            remove
+          </button>
+        </div>
+      )}
 
-        {banks.data?.map((bank: any) => (
-          <div key={bank.code} data-testid="bank">
-            <p>{bank.name}</p>
-            <p>{bank.code}</p>
-            <img src={bank.logo || DEFAULT_BANK_LOGO} alt={bank.name} width={30} height={30} />
-          </div>
-        ))}
-      </div>
+      {!currentBank && (
+        <div>
+          <h2>Select a bank</h2>
+
+          {banks.isLoading && <div data-testid="get-all-banks-loading">Loading banks...</div>}
+
+          {banks.data?.map((bank) => (
+            <Bank
+              key={bank.code}
+              bank={bank}
+              onClick={() => handleCurrentBankCodeChange(bank.code)}
+              dataTestid="bank"
+            />
+          ))}
+        </div>
+      )}
+
+      {verifyAccount.isLoading && <div data-testid="verify-account-loading">Verifying account...</div>}
+      {isRecipientFound && <div>{verifyAccount.data?.accountName}</div>}
+      {verifyAccount.isError && <div data-testid="verify-account-error">Error verifying account</div>}
+    </div>
+  );
+};
+
+const Bank = ({ dataTestid, bank, onClick }: { bank: TBank; onClick?: () => void; dataTestid?: string }) => {
+  return (
+    <div data-testid={dataTestid} onClick={onClick}>
+      <p>{bank.name}</p>
+      <p>{bank.code}</p>
+      <img src={bank.logo || DEFAULT_BANK_LOGO} alt={bank.name} width={30} height={30} />
     </div>
   );
 };
--- src/services/banks/index.ts
+++ src/services/banks/index.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "@utils/axiosInstance";
 import { ENDPOINTS } from "@constants/services";
-import { TBank } from "./types";
+import { TBank, TBankVerification } from "./types";
 
 const getBanks = async (): Promise<TBank[]> => {
   const { data: json } = await axiosInstance({
@@ -11,4 +11,19 @@ const getBanks = async (): Promise<TBank[]> => {
   return json.data as TBank[];
 };
 
-export { getBanks };
+const getBankVerification = async (accountNumber: string, bankCode: number): Promise<TBankVerification> => {
+  const { data: json } = await axiosInstance({
+    method: "GET",
+    url: ENDPOINTS.getBankVerify,
+    params: {
+      bank_account_number: accountNumber,
+      bank_code: bankCode,
+    },
+  });
+
+  return {
+    accountName: json.data.account_name,
+  };
+};
+
+export { getBanks, getBankVerification };
