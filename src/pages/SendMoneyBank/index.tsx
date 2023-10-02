30373a085ea9053d5973ac999833934635114a23
CreatorXperience <allyearmustobey@gmail.com>
2023-10-02 12:58:10 +0100

    ft-#74: refactor bank component
    Co-authored-by: StanleyOgada <stanleyogada2000@gmail.com>

--- src/pages/SendMoneyBank/index.tsx
+++ src/pages/SendMoneyBank/index.tsx
@@ -1,7 +1,9 @@
-import { DEFAULT_BANK_LOGO } from "@constants/index";
+import { COLORS, DEFAULT_BANK_LOGO } from "@constants/index";
 import useSendMoneyBank from "./hooks/useSendMoneyBank";
 import { TBank } from "@services/banks/types";
 import SendMoneyBeneficiaries from "@components/SendMoneyBeneficiaries";
+import styled from "styled-components";
+import PageNavHeader from "@components/PageNavHeader";
 
 const SendMoneyBank = () => {
   const {
@@ -21,89 +23,165 @@ const SendMoneyBank = () => {
   } = useSendMoneyBank();
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Recipient account number"
-        maxLength={10}
-        disabled={recipientAccountNumberInputIsDisabled}
-        {...register("recipientAccountNumber")}
-      />
+    <BankWrapper>
+      <PageNavHeader heading="Transfer to Bank Account" />
+      <div className="banner-wrapper">
+        <div className="banner">Instant, Zero issues,Free</div>
+      </div>
 
-      {currentBank && (
-        <div data-testid="current-bank">
-          <Bank bank={currentBank} />
+      <div className="bank-container">
+        <div className="input-wrapper">
+          <input
+            className="account-number-input"
+            type="text"
+            placeholder="Recipient account number"
+            maxLength={10}
+            disabled={recipientAccountNumberInputIsDisabled}
+            {...register("recipientAccountNumber")}
+          />
+        </div>
 
-          <button data-testid="remove" onClick={() => handleCurrentBankCodeChange(null)}>
-            remove
+        {currentBank && (
+          <div data-testid="current-bank">
+            <Bank bankName={currentBank.name} bankLogo={currentBank.logo} />
+
+            <button data-testid="remove" onClick={() => handleCurrentBankCodeChange(null)}>
+              remove
+            </button>
+          </div>
+        )}
+
+        {!currentBank && (
+          <div className="banks">
+            <h2 className="select">Select a bank</h2>
+
+            {banks.isLoading && <div data-testid="get-all-banks-loading">Loading banks...</div>}
+
+            {banks.data?.map((bank) => (
+              <Bank
+                key={bank.code}
+                bankLogo={bank.logo}
+                bankName={bank.name}
+                onClick={() => handleCurrentBankCodeChange(bank.code)}
+                dataTestid="bank"
+              />
+            ))}
+          </div>
+        )}
+
+        <form onSubmit={handleSendMoney()}>
+          <input
+            type="text"
+            placeholder="Amount"
+            {...register("amount", {
+              required: true,
+              min: 3,
+            })}
+          />
+          <input
+            type="text"
+            placeholder="Note"
+            {...register("remark", {
+              required: true,
+              min: 3,
+            })}
+          />
+
+          <button type="submit" disabled={isSendMoneyButtonDisabled}>
+            Send money
+            {sendMoneyMutation && sendMoneyMutation.isLoading && <div data-testid="loading">Sending money...</div>}
           </button>
-        </div>
-      )}
 
-      {!currentBank && (
-        <div>
-          <h2>Select a bank</h2>
+          {sendMoneyMutation.isError && <div data-testid="send-money-error">Error sending money</div>}
+        </form>
 
-          {banks.isLoading && <div data-testid="get-all-banks-loading">Loading banks...</div>}
+        {isRecipientFound && <div>{verifyAccount.data?.accountName}</div>}
+        {verifyAccount.isLoading && <div data-testid="verify-account-loading">Verifying account...</div>}
+        {verifyAccount.isError && <div data-testid="verify-account-error">Error verifying account</div>}
 
-          {banks.data?.map((bank) => (
-            <Bank
-              key={bank.code}
-              bank={bank}
-              onClick={() => handleCurrentBankCodeChange(bank.code)}
-              dataTestid="bank"
-            />
-          ))}
-        </div>
-      )}
-
-      <form onSubmit={handleSendMoney()}>
-        <input
-          type="text"
-          placeholder="Amount"
-          {...register("amount", {
-            required: true,
-            min: 3,
-          })}
+        <SendMoneyBeneficiaries
+          showBeneficiaries={showBeneficiaries}
+          beneficiaries={beneficiaries}
+          onBeneficiaryClick={handleBeneficiaryClick}
         />
-        <input
-          type="text"
-          placeholder="Note"
-          {...register("remark", {
-            required: true,
-            min: 3,
-          })}
-        />
-
-        <button type="submit" disabled={isSendMoneyButtonDisabled}>
-          Send money
-          {sendMoneyMutation && sendMoneyMutation.isLoading && <div data-testid="loading">Sending money...</div>}
-        </button>
-
-        {sendMoneyMutation.isError && <div data-testid="send-money-error">Error sending money</div>}
-      </form>
-
-      {isRecipientFound && <div>{verifyAccount.data?.accountName}</div>}
-      {verifyAccount.isLoading && <div data-testid="verify-account-loading">Verifying account...</div>}
-      {verifyAccount.isError && <div data-testid="verify-account-error">Error verifying account</div>}
-
-      <SendMoneyBeneficiaries
-        showBeneficiaries={showBeneficiaries}
-        beneficiaries={beneficiaries}
-        onBeneficiaryClick={handleBeneficiaryClick}
-      />
-    </div>
+      </div>
+    </BankWrapper>
   );
 };
 
-const Bank = ({ dataTestid, bank, onClick }: { bank: TBank; onClick?: () => void; dataTestid?: string }) => {
+const Bank = ({
+  dataTestid,
+  bankName,
+  bankLogo,
+  onClick,
+}: {
+  bankName: TBank["name"];
+  bankLogo: TBank["logo"];
+  onClick?: () => void;
+  dataTestid?: string;
+}) => {
+  const bank = {
+    name: bankName,
+    logo: bankLogo,
+  };
+
   return (
     <div data-testid={dataTestid} onClick={onClick}>
       <p>{bank.name}</p>
-      <p>{bank.code}</p>
+      {/* <p>{bank.code}</p> */}
       <img src={bank.logo || DEFAULT_BANK_LOGO} alt={bank.name} width={30} height={30} />
     </div>
   );
 };
 
 export default SendMoneyBank;
+
+const BankWrapper = styled.div`
+  width: 100%;
+  height: auto;
+  border: 1px solid red;
+
+  .banner-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    .banner {
+      width: 90%;
+      padding: 12px;
+      background: ${COLORS.lightBlue};
+      border-radius: 12px;
+      color: ${COLORS.blue};
+      font-weight: 400;
+    }
+  }
+
+  .bank-container {
+    width: 100%;
+
+    .input-wrapper {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+
+      .account-number-input {
+        width: 95%;
+        height: 40px;
+        border-radius: 5px;
+        padding: 0 10px;
+        margin-bottom: 10px;
+        margin: 20px 0px;
+        background-color: #fff;
+        border: 1px solid #ccc;
+      }
+    }
+
+    .banks {
+      .select {
+        font-size: 20px;
+        color: ${COLORS.black};
+        margin: 10px;
+      }
+    }
+  }
+`;
