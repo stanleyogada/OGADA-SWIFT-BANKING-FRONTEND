3bc34a618b933dc584ad33d5b01c07daa868a34a
RichCode <stanleyogada2000@gmail.com>
2023-10-02 14:08:14 +0100

    ft-#74: Refactor SendMoneyBeneficiaries component
    - Add padding styles and more classNames

--- src/components/SendMoneyBeneficiaries/index.tsx
+++ src/components/SendMoneyBeneficiaries/index.tsx
@@ -1,4 +1,5 @@
 import { TBeneficiary } from "@customTypes/Beneficiary";
+import SendMoneyBeneficiariesWrapper from "./SendMoneyBeneficiariesWrapper";
 
 type TProps = {
   showBeneficiaries: boolean;
@@ -10,28 +11,34 @@ const SendMoneyBeneficiaries = ({ showBeneficiaries, beneficiaries, onBeneficiar
   if (!showBeneficiaries) return null;
 
   return (
-    <div>
-      <div className="beneficiaries">
+    <SendMoneyBeneficiariesWrapper>
+      <div className="beneficiaries__title">
         <h3>Beneficiaries</h3>
       </div>
 
-      {!beneficiaries.length && <p>No Beneficiaries</p>}
-
-      {beneficiaries.map((beneficiary: TBeneficiary) => (
-        <div
-          className="user-block beneficiary"
-          key={beneficiary.accountNumber}
-          data-testid="beneficiary"
-          onClick={() => onBeneficiaryClick(beneficiary.accountNumber as string)}
-        >
-          <img className="user-image" src={beneficiary.avatar} alt="avatar" />
-          <div className="text-wrapper">
-            <p className="fullname">{beneficiary.accountNumber}</p>
-            <p className="phone">{beneficiary.fullName}</p>
-          </div>
+      {!beneficiaries.length && (
+        <div className="beneficiaries__no-beneficiaries">
+          <p>No Beneficiaries</p>
         </div>
-      ))}
-    </div>
+      )}
+
+      <div className="beneficiaries__list">
+        {beneficiaries.map((beneficiary: TBeneficiary) => (
+          <div
+            className="user-block beneficiary"
+            key={beneficiary.accountNumber}
+            data-testid="beneficiary"
+            onClick={() => onBeneficiaryClick(beneficiary.accountNumber as string)}
+          >
+            <img className="user-image" src={beneficiary.avatar} alt="avatar" />
+            <div className="text-wrapper">
+              <p className="fullname">{beneficiary.accountNumber}</p>
+              <p className="phone">{beneficiary.fullName}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </SendMoneyBeneficiariesWrapper>
   );
 };
 
